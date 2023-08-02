71acf5362b9d05b3b4d3a1e823edb23f97af364b
dopeldev <322kuroneko@gmail.com>
2023-08-02 00:16:26 -0500

    fix datetime

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # JWTAPP
 ## Description:
-A simple API Token generator
+A simple API JSON Web Tokens generator
+RFC 7519
 
-Tratar de usar solamente Standard Library
+[RFC 7519](https://datatracker.ietf.org/doc/html/rfc7519)
+
+Tratar de usar solamente Standard Library lo que no esta recomendando
 
 ## Tecnologies:
 FastAPI
--- jwt.py
+++ jwt.py
@@ -1,12 +1,11 @@
 # authentication logic
 # ----------------------------------------------------------------------
-from base64 import b64encode, urlsafe_b64encode, urlsafe_b64decode
+from base64 import b64encode, b64decode
 import hmac
 import json
-import time
 from hashlib import sha256
-from typing import Callable, Optional
-from datetime import timedelta
+from typing import Callable
+from datetime import timedelta, datetime
 from functools import wraps
 from fastapi import HTTPException, Request
 
@@ -14,18 +13,17 @@ JWT_ACCESS_TOKEN_EXPIRE_MINUTES = 30
 JWT_SECRET_KEY = "secret"
 JWT_ALGORITHM = "HS256"
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+def create_access_token(data: dict):
     """
     Create access token
     :param data: data to be encoded
-    :param expires_delta: expiration time
     :return: access token
     """
     header = {
         "alg": JWT_ALGORITHM,
         "typ": "JWT",
             }
-    data["exp"] = time.time() + timedelta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES).total_seconds()
+    data["exp"] = datetime.utcnow() + timedelta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
 
     encoded_header = b64encode(json.dumps(header).encode("utf-8")).decode("utf-8")
     encoded_payload = b64encode(json.dumps(data).encode("utf-8")).decode("utf-8")
@@ -35,21 +33,20 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
 
     return token
 
-def verify_token(token: str):
-    encoded_header, encoded_payload, signature = token.split(".")
+def verify_token(token: str, **payload: datetime) -> bool:
+    encoded_header, encoded_payload, received_signature = token.split(".")
 
     to_sign = f"{encoded_header}.{encoded_payload}".encode("utf-8")
-    signature_check = hmac.new(JWT_SECRET_KEY.encode("utf-8"), to_sign, sha256).hexdigest()
-    expected_signature = urlsafe_b64encode(signature_check.encode("utf-8")).decode("utf-8").rstrip("=")
+    expected_signature = hmac.new(JWT_SECRET_KEY.encode("utf-8"), to_sign, sha256).hexdigest()
 
-    if signature != expected_signature:
+    if received_signature != expected_signature:
         raise Exception("Invalid token")
 
-    payload = json.loads(urlsafe_b64decode(encoded_payload + "==="))
+    payload = json.loads(b64decode(encoded_payload).decode("utf-8"))
 
-    if time.time() > payload["exp"]:
+    if payload["exp"] < datetime.utcnow():
         raise Exception("Token expired")
-    return payload
+    return True
 
 def require_auth(func: Callable) -> Callable:
     """
